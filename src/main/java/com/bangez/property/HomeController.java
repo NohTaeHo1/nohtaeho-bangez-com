adb93345bf283b8b0ec0744fc97fde10a6ef2e45
noh_taeho <skysapal@gmail.com>
2024-06-13 12:22:13 +0900

    Hello World

---
+++ src/main/java/com/bangez/property/HomeController.java
@@ -0,0 +1,14 @@
+package com.bangez.property;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public Mono<String> home() {
+        return Mono.just("Hello World");
+    }
+}
